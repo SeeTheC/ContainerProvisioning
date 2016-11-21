be510ea21cccb3390fb5d964cb48ae9edf8ef1d1
Khursheed Ali <khursheed.ali1991@gmail.com>
2016-11-21 12:08:55 +0530

    Reading-Experiments-3

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 class Constants():
     MBtoKB = 1024;
     MemThresholdPercent = 0.8;
-    UGPercent=0.20 # underflow gap; More the value less we can take
-    OGPercent=0.05 # overflow gap; More the value Tough to resolve
+    UGPercent=0 # underflow gap; More the value less we can take
+    OGPercent=0 # overflow gap; More the value Tough to resolve
     MonitorDuration=15 # secs
